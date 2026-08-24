35a59fdfa18102f2f3c4da8c41f9ae9823d40fe4
Priyansh Soni <sonipriyansh6714@gmail.com>
2026-08-24 13:09:27 +0530

    First Commit

---
+++ JavaBasics.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class JavaBasics {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number : ");
+        int b = sc.nextInt();
+        System.out.println("Enter your operator : ");
+        char operator = sc.next().charAt(0);
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            case '%':
+                System.out.println(a % b);
+                break;
+            default:
+                System.out.println("wrong operator because My calculator is advance");
+        }
+        sc.close();
+    }
+}
